5b5eab3608343f7a9233249e17ee8a82107efd9a
minhhuynh12 <huynhcongminh12@gmail.com>
2019-05-15 13:20:21 +0700

    fix layout bill

--- lib/bill.dart
+++ lib/bill.dart
@@ -15,66 +15,132 @@ class BillPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
           child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               new Container(
-                padding: const EdgeInsets.all(20),
-                decoration: new BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: new BorderRadius.circular(8.0)),
-                child: new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                child: new Column(
                   children: <Widget>[
                     new Container(
-                      child: Row(
+                      padding: const EdgeInsets.all(20),
+                      decoration: new BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: new BorderRadius.circular(8.0)),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
                           new Container(
-                            height: 50.0,
-                            width: 50.0,
-                            decoration: BoxDecoration(
-                              color: Color(0x50FF9933),
-                              borderRadius: BorderRadius.all(Radius.circular(50))                              
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: ImageAsset(),
+                            child: Row(
+                              children: <Widget>[
+                                new Container(
+                                  height: 50.0,
+                                  width: 50.0,
+                                  decoration: BoxDecoration(
+                                      color: Color(0x50FF9933),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(50))),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: ImageAsset(),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8.0,
+                                ),
+                                Text(
+                                  "Cash dilivered",
+                                  style: new TextStyle(
+                                      fontSize: 16.0, color: Colors.orange),
+                                ),
+                              ],
                             ),
                           ),
-                          SizedBox(
-                            width: 8.0,
+                          new Container(
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  "100",
+                                  style: new TextStyle(
+                                      fontSize: 16.0,
+                                      color: Colors.orange,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 4.0),
+                                  child: Text(
+                                    " USD",
+                                    style: new TextStyle(
+                                        fontSize: 11.0,
+                                        color: Colors.orange,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
-                          Text(
-                            "Cash dilivered",
-                            style: new TextStyle(
-                                fontSize: 16.0, color: Colors.orange),
+                          new Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: new BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: new BorderRadius.circular(8.0)),
+                            child: new Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                new Container(
+                                  child: Row(
+                                    children: <Widget>[
+                                      Container(
+                                        width: 50.0,
+                                        height: 50.0,
+                                        decoration: BoxDecoration(
+                                            color: Color(0x5000C9E8),
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(50.0))),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(10.0),
+                                          child: ImageHolding(),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 8.0,
+                                      ),
+                                      Text(
+                                        "CamShip is Holding",
+                                        style: new TextStyle(
+                                            fontSize: 16.0, color: Colors.blue),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                new Container(
+                                  child: Row(
+                                    children: <Widget>[
+                                      Text(
+                                        "100",
+                                        style: new TextStyle(
+                                            fontSize: 16.0,
+                                            color: Colors.blue,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(top: 4.0),
+                                        child: Text(
+                                          " USD",
+                                          style: new TextStyle(
+                                              fontSize: 11.0,
+                                              color: Colors.blue,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ],
                       ),
                     ),
-                    new Container(
-                      child: Row(
-
-                        children: <Widget>[
-                          Text(
-                            "100",
-                            style: new TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.orange,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 4.0),
-                            child: Text(
-                              " USD",
-                              style: new TextStyle(
-                                  fontSize: 11.0,
-                                  color: Colors.orange,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        ],
-                      ),
-                    )
                   ],
                 ),
               ),
@@ -82,80 +148,18 @@ class BillPage extends StatelessWidget {
                 height: 16.0,
               ),
               new Container(
-                padding: const EdgeInsets.all(20),
-                decoration: new BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: new BorderRadius.circular(8.0)),
-                child: new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    new Container(
-                      child: Row(
-                        
-                        children: <Widget>[
-                          Container(
-                            width: 50.0,
-                            height: 50.0,
-                            decoration: BoxDecoration(
-                              
-                              color: Color(0x5000C9E8),
-                              borderRadius: BorderRadius.all(Radius.circular(50.0))
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: ImageHolding(),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 8.0,
-                          ),
-                          Text(
-                            "CamShip is Holding",
-                            style: new TextStyle(
-                                fontSize: 16.0, color: Colors.blue),
-                          ),
-                        ],
-                      ),
-                    ),
-                    new Container(
-                      child: Row(
-                        children: <Widget>[
-                          Text(
-                            "100",
-                            style: new TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 4.0),
-                            child: Text(
-                              " USD",
-                              style: new TextStyle(
-                                  fontSize: 11.0,
-                                  color: Colors.blue,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              new Container(
                 width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  //  borderRadius: BorderRadius.
+                padding: const EdgeInsets.all(3.0),
+                decoration: new BoxDecoration(
+                    border: new Border.all(color: Colors.orange, width: 2.0)),
+                child: new Text(
+                  "My Awesome Border",
+                  textAlign: TextAlign.center,
                 ),
-                child: Text("history"),
               )
             ],
           ),
-          
         ),
-       
       ),
     );
   }
@@ -178,7 +182,6 @@ class ImageHolding extends StatelessWidget {
     // TODO: implement build
 
     return Container(
-     
         child: Image.asset('icons/coin.png',
             height: 50.0, width: 50.0, fit: BoxFit.cover));
   }
